00c65d25d5861078869b2585fca6c42ca17023b4
DClark <duncan.clark528@gmail.com>
2025-08-20 14:54:08 -0400

    Added win Condition. Need tie

--- js/connect4app.js
+++ js/connect4app.js
@@ -16,19 +16,17 @@
 
 
 /*-------------------------------- Constants --------------------------------*/
-/*ADD WINNING COMBOS LIST HERE. MUST ITERATE OVER EACH OF THESE.*/
 
-const winningCombos = [['0_0', '0_1', '0_2', '0_3', '0_4', '0_5']];
 
 /*---------------------------- Variables (state) ----------------------------*/
 
 let board = [['', '', '', '', '', ''],
-            ['', '', '', '', '', ''],
-            ['', '', '', '', '', ''], 
-            ['', '', '', '', '', ''],
-            ['', '', '', '', '', ''],
-            ['', '', '', '', '', ''],
-            ['', '', '', '', '', '']];
+['', '', '', '', '', ''],
+['', '', '', '', '', ''],
+['', '', '', '', '', ''],
+['', '', '', '', '', ''],
+['', '', '', '', '', ''],
+['', '', '', '', '', '']];
 
 let turn;
 let winner = false;
@@ -65,13 +63,13 @@ const render = () => {
 
 //INIT BOARD
 function initialize() {
-   board = [['', '', '', '', '', ''],
-            ['', '', '', '', '', ''],
-            ['', '', '', '', '', ''], 
-            ['', '', '', '', '', ''],
-            ['', '', '', '', '', ''],
-            ['', '', '', '', '', ''],
-            ['', '', '', '', '', '']];
+    board = [['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', '']];
     turn = "R";
     messageEl.innerText = 'Let\'s Play!';
     //ADD RENDER
@@ -81,7 +79,7 @@ function initialize() {
 
 
 //RESET BOARD
-function resetBoard () {
+function resetBoard() {
     board = [['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', '']];
     turn = `R`;
     messageEl.innerText = `It's ${turn} teams move.`;
@@ -94,11 +92,12 @@ function resetBoard () {
 
 //UPDATE MESSAGE
 function updateMessage() {
-    if(winner === false && draw === false){
+    if (winner === false && draw === false) {
         messageEl.innerHTML = `It's ${turn} teams move.`;
-    }else if (winner === false && teamMoves.redTeam === 0 && teamMoves.blackTeam === 0) {
+    } else if (winner === false && teamMoves.redTeam === 0 && teamMoves.blackTeam === 0) {
         messageEl.innerHTML = "DRAW!"
-    }else { messageEl.innerHTML = `Congrats you won!`; //way to specify team? like congrats ${turn} team you won!
+    } else if (winner === true && draw === false) {
+        messageEl.innerHTML = `Congrats ${turn} team! You won!`; //way to specify team? like congrats ${turn} team you won!
     }
 }
 
@@ -113,13 +112,13 @@ function updateMessage() {
 // was changing. were mixing up our strings and DOMS. 
 
 function updateBoard() {
-    for (let col = 0; col < board.length; col++){
-        for (let row = 0; row < board[col].length; row++){
+    for (let col = 0; col < board.length; col++) {
+        for (let row = 0; row < board[col].length; row++) {
             const circleId = `${col}_${row}`; //necessary to take id of each Circle Div and convert it into a string. 
             // This is because HTML elements are strings. so getElementById (how we'll seelect our circles) will expect a string input.
             const circle = document.getElementById(circleId);//selects the circle we want based on its id. 
-            const circleValue = board [col][row];// Maps the actuall array of arrays to our board. 
-            if (circleValue === "R"){    //ANOTHET CHANGE, because we now have a map of our values called circleValue. You do not need the .innerText, because the value of board [col][row] will be either R or B.
+            const circleValue = board[col][row];// Maps the actuall array of arrays to our board. 
+            if (circleValue === "R") {  //ANOTHER CHANGE, because we now have a map of our values called circleValue. You do not need the .innerText, because the value of board [col][row] will be either R or B.
                 circle.style.backgroundColor = "red";
                 circle.style.color = "white";
             } else if (circleValue === "B") {
@@ -138,7 +137,7 @@ function updateBoard() {
 
 function switchPlayerTurn() {
     if (winner === false && draw === false) {
-        if (turn === 'R'){
+        if (turn === 'R') {
             turn = 'B';
         } else {
             turn = 'R';
@@ -147,14 +146,79 @@ function switchPlayerTurn() {
 }
 
 
+function deductPiece() {
+    
+}
+
+
+
+
+
+
+//CHECK FOR WINNER
 function checkForWinner() {
     for (let col = 0; col < board.length; col++) {
-        for (let row = 0; row <board[col].length; row++){
-            
+        for (let row = 0; row < board[col].length; row++) {
+            let currentCircle = board[col][row];
+            if (currentCircle === '') {
+                continue; // continue means to skip the current iteration of the loop. (i.e. if circle is empty, skip to next part) MDN.
+                //Check horizontal win.     
+            } if (col <= board.length - 4) { //NOTE ABOUT THIS LINE: cols - 4 means it will look at the next 3 columns and EXCLUDE the 4th. Its not subtracting 4 columns, just creating a hard stop at 3 by excluding the 4th column (it in the intro to arrays lesson)
+                let hCount = 1;
+                // console.log(hCount);
+                for (let i = 1; i <= 3; i++) {
+                    if (board[col + i][row] === currentCircle) { //chatGTP to debug this line. accidentally had a ==+ in this line.
+                        hCount += 1;
+                    } else {
+                        break;
+                    }
+                }
+                if (hCount === 4) {
+                    return winner = true;
+                }
+                // CHECK FOR VERT WIN. 
+            } if (row <= board[col].length - 4) {
+                let vCount = 1;
+                //console.log(vCount);
+                for (let i = 1; i <= 3; i++) {
+                    if (board[col][row + i] === currentCircle) {
+                        vCount += 1;
+                    } else {
+                        break;
+                    }
+                } if (vCount === 4) {
+                    return winner = true;
+                }
+                //CHECK DIAG DOWN RIGHT       
+            } if (col <= board.length - 4 && row <= board[col].length - 4) {
+                let rDownCount = 1;
+                for (let i = 1; i <= 3; i++) {
+                    if (board[col + i][row + i] === currentCircle) {
+                        rDownCount += 1;
+                    } else {
+                        break;
+                    }
+                } if (rDownCount === 4) {
+                    return winner = true;
+                }
+                //CHECK DIAG DOWN LEFT                 
+            } if (col >= 3 && row <= board[col].length - 4) { //NOTE col must be >= to 3 because we are checking backwards. Col is established to be the outer arry at 0 (our 0 column). You cant subtract from col because that would be -3 (out of bounds). SO you must be at least 3 columns away from outer columns. 
+                let lDownCount = 1;
+                for (let i = 1; i <= 3; i++) {
+                    if (board[col - i][row + i] === currentCircle) {
+                        lDownCount += 1;
+                    } else {
+                        break;
+                    }
+                } if (lDownCount === 4) {
+                    return winner = true;
+                }
+            }
         }
     }
 }
-    
+
+
 //CLICK FUNCTION // this is also your drop piece handler. 
 //Column has been clicked. You hav id for column. You want to use that
 //column Id to check if the row array has ANY values. If no value exists, the last
@@ -162,43 +226,47 @@ function checkForWinner() {
 // DOES have a value, then the next available spot at the END of the array should be styled. 
 
 const clickColumn = (event) => { /// this is your "handleclick".
-    let columnId = parseInt(event.currentTarget.id);
+    let columnId = parseInt(event.currentTarget.id); //Used chatGPT to debug this line. was getting an error depending on DOM element that was click (column vs circle). 
     let columnCircles = event.currentTarget.querySelectorAll('.circle');
-    console.log(columnId);
+    //console.log(columnId);
     //console.log('currentTarget:', event.currentTarget);
     //console.log('target', event.target);
-     if (columnCircles[5].innerText  === '') { 
-         board[columnId][5] = turn;
-         columnCircles[5].innerText = turn; 
-         console.log(board[columnId]);
-    } else if (columnCircles[4].innerText  === '') { 
-         board[columnId][4] = turn;
-         columnCircles[4].innerText = turn; 
-         console.log(board[columnId]);
-    } else if(columnCircles[3].innerText  === '') { 
-        board[columnId][3] = turn;
-        columnCircles[3].innerText = turn; 
+    if (columnCircles[5].innerText === '') {
+        board[columnId][5] = turn;
+        columnCircles[5].innerText = turn;
         console.log(board[columnId]);
-    } else if (columnCircles[2].innerText  === '') { 
+    } else if (columnCircles[4].innerText === '') {
+        board[columnId][4] = turn;
+        columnCircles[4].innerText = turn;
+        console.log(board[columnId]);
+    } else if (columnCircles[3].innerText === '') {
+        board[columnId][3] = turn;
+        columnCircles[3].innerText = turn;
+        console.log(board[columnId]);
+    } else if (columnCircles[2].innerText === '') {
         board[columnId][2] = turn;
         columnCircles[2].innerText = turn;// HAD TO UPDATE THIS TO ACTUALLY SEE THE UPDATED Values and CHANGE ARRAY. 
         console.log(board[columnId]);
-      //  columnCircles[2].innerText = turn; // will use this later
-    } else if (columnCircles[1].innerText === ''){
+        //  columnCircles[2].innerText = turn; // will use this later
+    } else if (columnCircles[1].innerText === '') {
         board[columnId][1] = turn;
         console.log(board[columnId]);
-         columnCircles[1].innerText = turn;
+        columnCircles[1].innerText = turn;
     } else if (columnCircles[0].innerText === '') {
-        board[columnId][0]= turn;
+        board[columnId][0] = turn;
         console.log(board[columnId]);
         columnCircles[0].innerText = turn;
-    } else if (columnCircles.innerText !== ''){
+    } else if (columnCircles.innerText !== '') {
         return;
+
     }
     updateBoard();
+    checkForWinner();
     switchPlayerTurn();
     updateMessage();
-}  
+}
+
+
 
 // WHY currentTarget.id was necessary:
 // currentTarget.id refers to the element the EVENT LISTENER IS ATTACHED TO. 
@@ -233,10 +301,10 @@ const clickColumn = (event) => { /// this is your "handleclick".
 /*----------------------------- Event Listeners -----------------------------*/
 //LOAD APP
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     initialize();
     columnEls.forEach((column) => {
-        column.addEventListener('click', clickColumn,); 
+        column.addEventListener('click', clickColumn,);
     });
 });
 
@@ -244,3 +312,15 @@ document.addEventListener('DOMContentLoaded', function(){
 restartButton.addEventListener('click', resetBoard);
 
 
+
+
+/*------------------------GRAVEYARD-----------------------------------------*/
+
+/*
+ADD WINNING COMBOS LIST HERE. MUST ITERATE OVER EACH OF THESE.
+const winningCombos = [['0_0', '0_1', '0_2', '0_3', '0_4', '0_5']];
+
+-DC no longer necessary. Went different route for check win condition based on Jan's suggestion. 
+
+
+*/
